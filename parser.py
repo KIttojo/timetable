757c7ac8ff7f29a65650888c738717939320b468
KIttojo <45813478+KIttojo@users.noreply.github.com>
2019-11-04 22:22:48 +0300

    Update parser.py

--- parser.py
+++ parser.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 import urllib.request
 try: 
     from BeautifulSoup import BeautifulSoup
@@ -5,28 +7,34 @@ except ImportError:
     from bs4 import BeautifulSoup
 
 with urllib.request.urlopen('http://ictis.sfedu.ru/rasp/HTML/39.htm') as url: s = url.read()
-
 soup = BeautifulSoup(s, 'html.parser')
+#code starts below
+# needed_day = 'Птн,27  сентября '
+day_file = open('C:\\Users\\denis\\Desktop\\Python 3\\parser\\day.txt', 'r', encoding='utf-8')
+needed_day = day_file.readline()
+print(needed_day)
 
-needed_day = 'Птн,01  ноября '
-#нахождение дня денели и запись в переменную
 table = soup.findAll('font', attrs={'size': '2'})
-# print(table)
 
-for x in table: #тут мы получили список дат. Ищем нужную, берем индекс и выводим дни, используя индекс
-	time_table = ''
-	day = x.find('p').text
-	if day == needed_day:
-		time_table = x
-		indx = table.index(time_table)
-		print(indx) #доставем индекс элемента, чтобы потом достать его из 119 записей
-	print(f'day={day}, needed_day={needed_day}')
+for x in table: #finding index of needed week and saving it in "indx" variable
+    time_table = ''
+    day = x.find('p').text
 
-for i in range(1):
- 	# print(soup.findAll('tr', attrs={'class': 'question-hyperlink'}, limit=2)) вывод предметов в один день
- 	name_box2 = soup.findAll('font', attrs={'size': "1"})
- 	print(name_box2)
- 	for x in range(indx*8, indx*8 + 6):
- 		buff = name_box2[x]
- 		buff = buff.text.strip()
- 		print(buff)
+    if day == needed_day:
+        time_table = x
+        indx = table.index(time_table)
+        break
+#if we found date, we'll save timetable for this day
+try:
+    file = open('C:\\Users\\denis\\Desktop\\Python 3\\parser\\timetable.txt', 'w')
+    name_box2 = soup.findAll('font', attrs={'size': "1"})
+
+    for x in name_box2[indx*9:indx*9+8]:
+        buff = x
+        buff = buff.text.strip()
+
+        file.write(buff)
+        print(buff)
+
+except:
+    print("Введите корректный день.")

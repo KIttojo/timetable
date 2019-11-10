8b6d1d6ba10068d568d80618ecae7e33613cdb7c
KIttojo <45813478+KIttojo@users.noreply.github.com>
2019-11-10 21:49:05 +0300

    Update vk_group_analysator_saved2.py

--- vk_group_analysator_saved2.py
+++ vk_group_analysator_saved2.py
@@ -1,9 +1,9 @@
 import vk_api
-import time
 import random
+import file1
 
 def main():
-    VK = vk_api.VkApi(token = 'aa56c8233e0b89dd0e67cd1cb04bcffaa791606ae2c039abd1af4f40306b543afa3cd308c8c87d87b9317')
+    VK = vk_api.VkApi(token = '7bae9726b9b3b04e6c21694562aa1e0f80e0dc0f202afae626f685b9cac3e4021e9aa077e48401c872dbc')
 
     while True:
         try:
@@ -15,14 +15,25 @@ def main():
             text = open('C:\\Users\\denis\\Desktop\\Python 3\\parser\\timetable.txt', 'r')
             day = open('C:\\Users\\denis\\Desktop\\Python 3\\parser\\day.txt', 'w', encoding='utf-8')
 
-            day.write(now_text)
+            space_indx = list(now_text).index(" ")
+            if space_indx<2: #if we have date less then 10 (1-9 days of every mounth)
+                now_text = "0" + now_text #here we got 01-09 day
 
-            # print(VK.method("messages.send", {"random_id": first_mes_1,"user_id": get_id_1, "message": text}))
+            for x in range(len(now_text)):
+                if now_text[x] == " ":
+                    formated_text = now_text[:x] + " " + now_text[x:] + " "
 
+            day.write(formated_text)
+            file1.main_f()
 
+            file = open('C:\\Users\\denis\\Desktop\\Python 3\\parser\\timetable.txt', 'r')
+            file_text = file.readline()
+            # print(file_text)
+            print(VK.method("messages.send", {"random_id": first_mes_1,"user_id": get_id_1, "message": file_text}))
 
         except:
             print("Imput is empty")
+            
 
 
 if __name__ == '__main__':

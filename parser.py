4b46bb791a83033ccc5e5fec63c75acc7daad436
KIttojo <45813478+KIttojo@users.noreply.github.com>
2020-02-12 22:07:21 +0300

    Release 1.0

--- parser.py
+++ parser.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 import urllib.request
-import vk_api
+
 try: 
     from BeautifulSoup import BeautifulSoup
 except ImportError:
@@ -9,55 +9,49 @@ with urllib.request.urlopen('http://ictis.sfedu.ru/rasp/HTML/39.htm') as url: s 
 
 def find_day(soup, needed_day):
 	print("\nfinding day")
-	# soup = BeautifulSoup(s, 'html.parser')
-	# day_file = open('C:\\Users\\denis\\Desktop\\Python 3\\parser\\day.txt', 'r', encoding='utf-8')
-	# needed_day = day_file.readline()
 
 	table = soup.findAll('font', attrs={'size': '2'})
+	matches = 0;
+	#находим индекс нужной нам недели
+	for x in table: 
 
-	for x in table: #finding index of needed week and saving it in "indx" variable
 		time_table = ''
 		day = x.find('p').text
 
 		if day[4:] == needed_day:
 			time_table = x
 			indx = table.index(time_table)
+			matches+=1
 			break
-		else: 
-			error = "Попробуйте ввести другой день."
 
-	find_timetable(soup, indx, error)
+	find_timetable(soup, indx, matches)
 
-def find_timetable(soup, indx, error):
-	print("finding timetable")
-	if (error != None):
-		print("День не НаН")
-		file = open('C:\\Users\\denis\\Desktop\\Python 3\\parser\\timetable.txt', 'w')
-		try:
-			name_box2 = soup.findAll('font', attrs={'size': "1"})
+def find_timetable(soup, indx, matches):
+	#используя найденный нами индекс, вытаскиваем расписание на этот день
+	file = open('DIR\\timetable.txt', 'w')
+	
+	try:
+		name_box2 = soup.findAll('font', attrs={'size': "1"})
 
-			for x in name_box2[indx*9:indx*9+8]:
-				buff = x
-				buff = buff.text.strip()
-				buff = buff.replace("_", "") #to delete "_" from the end of line
-				print(buff)
+		for x in name_box2[indx*9:indx*9+8]:
+			buff = x
+			buff = buff.text.strip()
+			buff = buff.replace("_", "") #удаляем "_" с конца строк
 
-				file.write(buff)
+			file.write("|  ") #чтобы отделять расписание пар
+			file.write(buff)
 
-				print(buff)
-		except:
-			file.write("Пар нет, отдыхайте.")
-
-	else:
-		print("Неверная дата")
-		file.write(error)
+	except:
+		file.write("Пар нет, отдыхайте.")
 
 	file.close()
 
 
 def main_f():
 	soup = BeautifulSoup(s, 'html.parser')
-	day_file = open('C:\\Users\\denis\\Desktop\\Python 3\\parser\\day.txt', 'r', encoding='utf-8')
+
+	day_file = open('DIR\day.txt', 'r', encoding='utf-8')
 	needed_day = day_file.readline()
+
 	find_day(soup, needed_day)
 	day_file.close()
